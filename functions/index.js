dfccaefc56cd676f0c0d6021ca3706f7c2a842df
nathan-kane <nkane1234@gmail.com>
2023-12-12 09:23:03 -0700

    Added mortgage calculator and made some fixes

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,32 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-
 admin.initializeApp();
 
-// Cloud Firestore triggers ref: https://firebase.google.com/docs/functions/firestore-events
+exports.updateTrxnNotification = functions.firestore
+    .document("company/{companyId}/trxns/{trxnsId}")
+    .onUpdate((change, context) => {
+      const newValue = change.after.data();
+      const documentId = context.params.documentId;
 
-exports.deleteTrxnNotification = functions.firestore
-    .document("/company/{companyId}/trxns/{trxnsId}")
-    .onDelete((snapshot, context) => {
-      const newData = snapshot.data();
-      const trxn = context.params.trxnsId;
+      // // Access other fields as needed from newValue
+
+      // // Constructing the notification payload
       const payload = {
         notification: {
-          title: String(newData.title),
-          body: String(newData.body),
+          title: "Document Updated",
+          body: "Document ${documentId} has been updated.",
         },
-        topic: trxn,
+        // You can add additional data fields as needed
+        data: {
+          documentId: documentId,
+        },
       };
-      return admin.messaging().send(payload);
+
+      // // Get the device token from your Firestore document
+      const deviceToken = newValue.deviceToken;
+
+      // Send the notification
+      return admin.messaging().send(deviceToken, payload);
     });
+
 
---
+++ lib/screens/mortgage_calculator.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class MortgageCalculatorScreen extends StatefulWidget {
+  const MortgageCalculatorScreen({super.key});
+
+  @override
+  _MortgageCalculatorScreenState createState() =>
+      _MortgageCalculatorScreenState();
+}
+
+class _MortgageCalculatorScreenState extends State<MortgageCalculatorScreen> {
+  double loanAmount = 100000;
+  double interestRate = 5.0;
+  int loanTerm = 30;
+  double homeOwnersInsurance = 0.0;
+  double taxesAndOtherFees = 0.0;
+  double hOA = 0.0;
+  double monthlyPayment = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mortgage Calculator'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Loan Amount (\$)'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  loanAmount = double.parse(value);
+                });
+              },
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Interest Rate (%)'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  interestRate = double.parse(value);
+                });
+              },
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Loan Term (years)'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  loanTerm = int.parse(value);
+                });
+              },
+            ),
+            TextField(
+              decoration:
+                  const InputDecoration(labelText: 'Homeowners Insurance'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  homeOwnersInsurance = double.parse(value);
+                });
+              },
+            ),
+            TextField(
+              decoration:
+                  const InputDecoration(labelText: 'Taxes and other fees'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  taxesAndOtherFees = double.parse(value);
+                });
+              },
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'HOA'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  hOA = double.parse(value);
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: calculatePayment,
+              child: const Text('Calculate'),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Monthly Payment: \$${monthlyPayment.toStringAsFixed(2)}',
+              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void calculatePayment() {
+    double monthlyInterestRate = interestRate / 1200;
+    int numberOfPayments = loanTerm * 12;
+
+    //double denominator = (1 - (1 / (1 + monthlyInterestRate).pow(numberOfPayments)));
+    double denominator =
+        (1 - (1 / pow(1 + monthlyInterestRate, numberOfPayments)));
+
+    monthlyPayment = ((loanAmount * monthlyInterestRate) / denominator) +
+        homeOwnersInsurance +
+        taxesAndOtherFees +
+        hOA;
+
+    setState(() {});
+  }
+}
